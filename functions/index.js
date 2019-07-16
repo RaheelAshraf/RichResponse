1f66db9f6eccc8c1d92c3c568299dd8a7e1063de
iamraheelashraf <labkoat@gmail.com>
2019-07-17 04:10:38 +0500

    code cleaned up

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,12 @@
 'use strict';
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
-const {Payload } = require('dialogflow-fulfillment');
-const Card = require('./intents/cardIntent'); 
-const Button = require('./intents/buttonIntent'); 
-const Suggestion = require('./intents/suggestionIntent'); 
-const Video = require('./intents/videoIntent'); 
-const Website = require('./intents/websiteIntent');  
-const QuickReplies = require('./intents/quickReplies'); 
+const Card = require('./intents/cardIntent');
+const Button = require('./intents/buttonIntent');
+const Suggestion = require('./intents/suggestionIntent');
+const Video = require('./intents/videoIntent');
+const Website = require('./intents/websiteIntent');
+const QuickReplies = require('./intents/quickReplies');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
@@ -27,28 +26,28 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
   const card = (agent) => {
-    Card.cardFun(agent); 
+    Card.cardFun(agent);
   }
 
 
   const Buttons = (agent) => {
-    Button.butFunc(agent); 
+    Button.butFunc(agent);
   }
 
   const suggestion = (agent) => {
-   Suggestion.suggFunc(agent); 
+    Suggestion.suggFunc(agent);
   }
 
   const video = (agent) => {
-    Video.videoFunc(agent); 
+    Video.videoFunc(agent);
   }
 
   const website = (agent) => {
-    Website.websiteFunc(agent); 
+    Website.websiteFunc(agent);
   }
 
   const quickReplies = (agent) => {
-    QuickReplies.quickRepFunc(agent); 
+    QuickReplies.quickRepFunc(agent);
   }
 
   let intentMap = new Map();
